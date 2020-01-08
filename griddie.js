c30dd4c3da16da80f2c9f8ba855c632135380ef2
Tommyhetrick <tommyhetrick@gmail.com>
2020-01-08 10:30:58 -0500

    Create griddie.js

---
+++ griddie.js
@@ -0,0 +1,15 @@
+// Griddie.js
+// Tom Hetrick Jr.
+
+function createGrid(tx,ty,options) {
+  return new Griddie(tx,ty,options);
+}
+
+class Griddie {
+  constructor(tx,ty,options) {
+    this.tx = tx;
+    this.ty = ty;
+    this.options = options;
+  }
+
+}
